fd5e774305bae43c6d6a58d5fe46aa197d8d6247
suraimu752 <suraimu752@gmail.com>
2022-01-03 05:55:21 +0900

    Major renovation

--- macroMaker/script.js
+++ macroMaker/script.js
@@ -3,25 +3,35 @@ waits["真価"] = " <wait.3>";
 waits["倹約"] = " <wait.2>";
 waits["長期倹約"] = " <wait.2>";
 waits["マスターズメンド"] = " <wait.3>";
+waits["マニピュレーション"] = "<wait.2>";
 waits["ヴェネレーション"] = " <wait.2>";
 waits["イノベーション"] = " <wait.2>";
+waits["グレートストライド"] = " <wait.2>";
 waits["作業"] = " <wait.3>";
 waits["模範作業"] = " <wait.3>";
 waits["下地作業"] = " <wait.3>";
 waits["倹約作業"] = " <wait.3>";
+waits["精密作業"] = "<wait.3>";
 waits["ヘイスティタッチ"] = " <wait.3>";
 waits["加工"] = " <wait.3>";
 waits["中級加工"] = " <wait.3>";
 waits["上級加工"] = " <wait.3>";
 waits["下地加工"] = " <wait.3>";
+waits["匠の早業"] = "<wait.3>";
 waits["ビエルゴの祝福"] = " <wait.3>";
-waits["グレートストライド"] = " <wait.2>";
+waits["突貫作業"] = " <wait.3>";
+waits["経過観察"] = " <wait.2>";
+waits["アート・オブ・エレメンタル"] = " <wait.2>";
+waits["ブランド・オブ・エレメンタル"] = " <wait.3>";
+waits["最終確認"] = " <wait.2>";
+waits["確信"] = " <wait.3>";
+waits["注視加工"] = " <wait.3>";
+waits["注視作業"] = " <wait.3>";
 // waits[""] = " <wait.>";
-
-let charaName = "Natsu Slime";
 
 function make(){
     let log = document.getElementById("logs").value;
+    let charaName = document.getElementById("name").value;
     let logs = log.split("\n");
 
     for(let i in logs){
@@ -36,15 +46,15 @@ function make(){
     }
 
     for(let i in skills){
-        console.log(skills[i]);
         skills[i] = skills[i].slice(charaName.length + 2, -7);
     }
 
     let output = [];
     for(let i in skills){
         if(waits[skills[i]] == undefined){
-            output = "待ち時間が設定されてないスキルが見つかりました\nwaits配列に追加してください\n" + skills[i];
-            break;
+            // document.getElementById("macro").value = "待ち時間が設定されてないスキルが見つかりました！\nwaits配列に追加してください！\n\"" + skills[i] + "\"";
+            document.getElementById("macro").value = "名前の入力が間違っているか、対応していないスキルが使用されている可能性があります。\n入力している名前がチャットに表示されている名前と同じかどうかお確かめください。"
+            return;
         }
         else{
             output.push("/ac " + skills[i] + waits[skills[i]] + "\n");
@@ -57,7 +67,6 @@ function make(){
 
     output.push("/e 完成！！！！！！<se.8>");
 
-    console.log(output);
 
     if(skills.length > 15){
         document.getElementById("macroWrapper").innerHTML = 
@@ -69,8 +78,17 @@ function make(){
         document.getElementById("macro2").innerHTML = output.slice(15).join("");
     }
     else{
+        document.getElementById("macroWrapper").innerHTML = 
+        '<textarea id="macro" cols="60" rows="42"></textarea>';
         document.getElementById("macro").value = output.join("");
     }
+}
+
+function autoNameComplete(){
+    let log = document.getElementById("logs").value;
+    log = log.split("\n")[0];
+    let charaName = log.split("]")[1].slice(1).split("の")[0];
+    document.getElementById("name").value = charaName;
 }
 
 const textarea = $("#logs");
@@ -80,6 +98,9 @@ textarea.on('keyup change', function () {
     let letterLength = textarea.val().length;
     if (letterLength !== 0) {
         placeholderDiv.addClass('none');
+
+        autoNameComplete();
+        make();
     } else {
         placeholderDiv.removeClass('none');
     }
